b2ef2a21d6c19f563306f6b7b0f2fe78a104c21e
Anne Munoz <anne@sevensols.com>
2018-12-17 08:56:01 +0100

    Redirect homepage

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # models start with capitals
@@ -10,6 +11,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
 
     # careful, each function inside class needs
     # indentation tab to belong to class
